4e6a0afdbde5dee793eeb4818b096e91721a443c
Realtent <realtentwo@gmail.com>
2023-10-03 14:05:20 -0600

    Separated into methods and added javadoc comments

--- src/TimingTester.java
+++ src/TimingTester.java
@@ -3,39 +3,70 @@
  *
  * @author Nicholas Un, Gabe Brown
  * @version 1.0, 9/20/23
- * @see int[], java.util.Random
+ * @see int[], Math.random()
  */
-import java.util.Random;
 public class TimingTester {
-    // length of list generated
-    public static void main(String[] args) {
-        int arrLength = 500000;
+    /**
+     * Makes a random array with length elements with values ranging from 0 to range
+     *
+     * @param elements how many elements going to be in the list
+     * @param range upper bound for how big the elements will be in the list
+     * @return randomized list under parameters elements and range
+     */
+    public static int[] generateRandList(int elements, int range){
         //generates list with length arrLength
-        int[] arr = new int[arrLength];
-        for(int i = 0; i<arrLength;i++) {
-            arr[i] = (int) (Math.random() * arrLength);
+        int[] arr = new int[elements];
+        for(int i = 0; i < elements;i++) {
+            arr[i] = (int) (Math.random() * range);
         }
-        // gets time before running the sorts, runs the sorts, gets time after and subtracts the before from after to get how much time it took
-        long beforeTime, afterTime;
-        //bubbleSort Time Test
-        beforeTime = System.currentTimeMillis();
-        QuadSorts.bubbleSort(arr);
-        afterTime = System.currentTimeMillis();
-        System.out.println("bubbleSort took " + (afterTime - beforeTime) + " milliseconds with a list length of " + arrLength + ".");
-        // selectionSort Time Test
-        beforeTime = System.currentTimeMillis();
-        QuadSorts.selectionSort(arr);
-        afterTime = System.currentTimeMillis();
-        System.out.println("selectionSort took " + (afterTime - beforeTime) + " milliseconds with a list length of " + arrLength + ".");
-        // insertionSort Time Test
-        beforeTime = System.currentTimeMillis();
-        QuadSorts.insertionSort(arr);
-        afterTime = System.currentTimeMillis();
-        System.out.println("insertionSort took " + (afterTime - beforeTime) + " milliseconds with a list length of " + arrLength + ".");
-        // mergeSort Time Test
-        beforeTime = System.currentTimeMillis();
-        QuadSorts.mergeSort(arr);
-        afterTime = System.currentTimeMillis();
-        System.out.println("mergeSort took " + (afterTime - beforeTime) + " milliseconds with a list length of " + arrLength + ".");
+        return arr;
+    }
+
+    /**
+     * Runs sorting algorithm sortID with randArr and returns how long it took
+     *
+     * @param randArr input random array
+     * @param sortID which sort algorithm will be tested
+     * @return how many milliseconds had passed after sort algorithm ran
+     */
+    public static int methodTimed(int[] randArr, int sortID){
+        long beforeTime = System.currentTimeMillis();
+        if(sortID == 0){
+            QuadSorts.bubbleSort(randArr);
+        } else if(sortID == 1){
+            QuadSorts.selectionSort(randArr);
+        } else if(sortID == 2){
+            QuadSorts.insertionSort(randArr);
+        } else if(sortID == 3){
+            QuadSorts.mergeSort(randArr);
+        }
+        long afterTime = System.currentTimeMillis();
+        return (int)(afterTime - beforeTime);
+    }
+
+    /**
+     * Identifies which sorting algorithm corresponds to sortID and returns it in string form
+     *
+     * @param sortID which sorting algorithm was run
+     * @return which Sort was run
+     */
+    public static String sortType(int sortID){
+        String sortType = "";
+        if(sortID == 0){
+            sortType = "bubbleSort";
+        } else if(sortID == 1){
+            sortType = "selectionSort";
+        } else if(sortID == 2){
+            sortType = "insertionSort";
+        } else if(sortID == 3){
+            sortType = "mergeSort";
+        }
+        return sortType;
+    }
+    public static void main(String[] args) {
+        int[] arr = generateRandList(100, 100);
+        for (int i = 0; i < 4; i++){
+            System.out.println("Algorithm " + sortType(i) + " finished in " + methodTimed(arr, i) + " milliseconds.");
+        }
     }
 }
